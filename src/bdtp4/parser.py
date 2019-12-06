9b6b51962424cc78bf554cca2000818048ae83b3
Mard <mardoqueu.pimentel@gmail.com>
2019-12-05 21:16:42 -0400

    remove unused code, fix unnecessary bytes object and parse times

--- src/bdtp4/parser.py
+++ src/bdtp4/parser.py
@@ -1,54 +1,57 @@
+import datetime
+
+
 def parse(filename):
 	IGNORE_FIELDS = ['Total items', 'reviews']
-	f = open(filename, 'r', encoding='utf-8')
 	entry = {}
 	categories = []
 	reviews = []
-	similar_items = []
 
-	for line in f:
-		line = line.strip()
-		colonPos = line.find(':')
+	with open(filename) as file:
 
-		if line.startswith("Id"):
-			if reviews:
-				entry["reviews"] = reviews
-			if categories:
-				entry["categories"] = categories
-			yield entry
-			entry = {}
-			categories = []
-			reviews = []
-			rest = line[colonPos + 2:]
-			entry["id"] = rest.strip().encode('utf8', 'ignore')
-		#             entry["id"] = unicode(rest.strip(), errors='ignore')
+		for line in file:
+			line = line.strip()
+			colonPos = line.find(':')
 
-		elif line.startswith("similar"):
-			similar_items = line.split()[2:]
-			entry['similar_items'] = similar_items
+			if line.startswith('Id'):
+				if reviews:
+					entry['reviews'] = reviews
+				if categories:
+					entry['categories'] = categories
+				yield entry
+				entry = {}
+				categories = []
+				reviews = []
+				rest = line[colonPos + 2:]
+				entry['id'] = rest.strip()
 
-		# "cutomer" is typo of "customer" in original data
-		elif line.find("cutomer:") != -1:
-			review_info = line.split()
-			reviews.append({'customer_id': review_info[2],
-											'rating': int(review_info[4]),
-											'votes': int(review_info[6]),
-											'helpful': int(review_info[8])})
+			elif line.startswith('similar'):
+				similar_items = line.split()[2:]
+				entry['similar'] = similar_items
 
-		elif line.startswith("|"):
-			categories.append(line)
+			elif line.find('cutomer:') != -1:
+				review_info = line.split()
+				reviews.append({
+					'time': datetime.datetime.strptime(review_info[0], "%Y-%m-%d").timestamp(),
+					'customer_id': review_info[2],
+					'rating': int(review_info[4]),
+					'votes': int(review_info[6]),
+					'helpful': int(review_info[8])
+				})
 
-		elif colonPos != -1:
-			eName = line[:colonPos]
-			rest = line[colonPos + 2:]
+			elif line.startswith('|'):
+				categories.append(line)
 
-			if not eName in IGNORE_FIELDS:
-				entry[eName] = rest.strip().encode('utf8', 'ignore')
-	#                 entry[eName] = unicode(rest.strip(), errors='ignore')
+			elif colonPos != -1:
+				eName = line[:colonPos]
+				rest = line[colonPos + 2:]
+
+				if eName not in IGNORE_FIELDS:
+					entry[eName] = rest.strip()
 
 	if reviews:
-		entry["reviews"] = reviews
+		entry['reviews'] = reviews
 	if categories:
-		entry["categories"] = categories
+		entry['categories'] = categories
 
 	yield entry
